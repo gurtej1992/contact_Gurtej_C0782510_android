8ba3da49c42e1d337d67dcaa4a3a9d3310a17a03
Tej <codewithtej@gmail.com>
2021-02-23 08:26:23 -0500

    Initial commit

---
+++ app/src/main/java/com/lambton/contact_gurtej_c0782510_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lambton.contact_gurtej_c0782510_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
